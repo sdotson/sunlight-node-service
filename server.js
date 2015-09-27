4f30e47f30dfe39bc59301450eaf92236cf357ac
Stu <stuart@blueprintinteractive.com>
2015-09-27 12:33:22 -0400

    route now echoing json from mongodb

--- server.js
+++ server.js
@@ -18,39 +18,19 @@ MongoClient.connect("mongodb://localhost:27017/sunlight", function(err, database
 
   // Start the application after the database connection is ready
   app.listen(8080);
-  console.log("Listening on port 3000");
+  console.log("Listening on port 8080");
 });
 
 // Reuse database object in request handlers
 app.get("/", function(req, res) {
-    var json = [];
 
   db.collection("candidates").find({}, function(err, docs) {
     docs.each(function(err, doc) {
       if(doc) {
-        json.push(json);
-      }
-      else {
+        res.json(doc);
+      } else {
         res.end();
-      }
+      };
     });
-    res.send(results);
   });
 });
-
-
-
-
-
-/*app.get('/candidates', function(request, response) {
-
-    MongoClient.connect(url, function(err, db) {
-
-        var data = db.collection('candidates').find();
-        console.log(data)
-        db.close();
-      
-    });
-});
-
-app.listen(8080);*/
