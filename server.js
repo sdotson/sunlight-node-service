dd795fff54a026f3b0564af3f7059ee83870bf9e
Stu <stuart@blueprintinteractive.com>
2015-09-27 16:58:54 -0400

    everything working. success

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var url = 'mongodb://localhost:27017/sunlight';
 var jobSchedule = require('./job-schedule.js');
 jobSchedule.setupJobs();
 
-
 var db;
 
 // Initialize connection once
@@ -21,13 +20,19 @@ MongoClient.connect("mongodb://localhost:27017/sunlight", function(err, database
   console.log("Listening on port 8080");
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Reuse database object in request handlers
 app.get("/", function(req, res) {
 
   db.collection("candidates").find({}, function(err, docs) {
     docs.each(function(err, doc) {
       if(doc) {
-        res.json(doc);
+        res.jsonp(doc.data);
       } else {
         res.end();
       };
